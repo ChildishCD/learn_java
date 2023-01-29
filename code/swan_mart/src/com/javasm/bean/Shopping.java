60f8b8dd43791cb76527b3448802aa6256e44910
ChildishCD <childishwickedcd@gmail.com>
2023-01-30 00:29:53 +0800

    order related update

---
+++ code/swan_mart/src/com/javasm/bean/Shopping.java
@@ -0,0 +1,13 @@
+package com.javasm.bean;
+
+import lombok.AllArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@AllArgsConstructor
+public class Shopping{
+    String name;
+    Integer count;
+    ProductsModel product;
+}
